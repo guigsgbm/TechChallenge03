8be9a486388f933042e1ce106905e13c61ede568
unknown <guigsgbm@gmail.com>
2023-11-01 19:27:19 -0300

    update dockerfile

--- tests/TestProject1/NewsAPIUnitTest.cs
+++ tests/TestProject1/NewsAPIUnitTest.cs
@@ -17,33 +17,5 @@ public class NewsAPIUnitTest
     [TestMethod]
     public async Task Uploadnews_ValidModel_ReturnsOkResult()
     {
-        var contextMock = new Mock<AppIdentityDbContext>();
-        var newsRepositoryMock = new Mock<NewsRepository>(contextMock.Object);
-
-        var userManager = new Mock<UserManager<IdentityUser>>();
-
-        var mapper = new Mock<IMapper>();
-        var newsMapper = new NewsMapper(mapper.Object);
-        var controller = new NewsController(newsRepositoryMock.Object, newsMapper, userManager.Object);
-
-        var createNewsDto = new CreateNewsDto
-        {
-            // Preencha os campos do DTO com dados válidos para o teste
-        };
-
-        var user = new IdentityUser
-        {
-            // Defina as propriedades do usuário, como o ID
-        };
-
-        userManager.Setup(um => um.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
-
-        // Act
-        var result = await controller.Uploadnews(createNewsDto);
-
-        // Assert
-        var okResult = result as OkObjectResult;
-        Assert.IsNotNull(okResult);
-        Assert.AreEqual(200, okResult.StatusCode);
     }
 }
